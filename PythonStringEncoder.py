3a1f9b8b51c826259ec9f305834fb081373d5a60
Locyst <154724542+Locyst@users.noreply.github.com>
2024-02-14 12:13:36 -0700

    Update PythonStringEncoder.py
    Added the ability to reinforce the encryption strength and added documentation. Will improve variable names later

--- PythonStringEncoder.py
+++ PythonStringEncoder.py
@@ -19,22 +19,42 @@ def reverse(string):
   
   return returnString
 
-def createSeed():
-  capitals = random.randint(0, 9)
-  backs = random.randint(0, 9)
-  fronts = random.randint(0, 9)
-  extra = random.randint(0, 9)
+def createSeed(securityLevel=1):
+  """
+  Creates a seed used for encoding
+
+  Parameters:
+   - securityLevel (int): The higher the number the stronger the
+   seed is. Do not input decimals ( Default is 1 )
+
+  Returns:
+   List: A list that has where the captials are, how far back to
+   go, how far forwards to go and an extra
+  """
+  capitals = random.randint(0, 9 * securityLevel)
+  backs = random.randint(0, 9 * securityLevel)
+  fronts = random.randint(0, 9 * securityLevel)
+  extra = random.randint(0, 9 * securityLevel)
 
   return [capitals, backs, fronts, extra]
 
 def encode(string, seed=None):
+  """
+  Encodes a string using a custom encryption method
+
+  Paramaters:
+   - string (Str): The string going through the encryption method
+   - seed (List[int]): The seed being used for encryption
+
+  Return:
+   - Str: The encoded string
+  """
   if seed is None:
     seed = createSeed()
+    print(f'Creating encoded message using seed: {seed}')
   encoded = []
 
   encoded.append("+")
-
-  print(f'Creating encoded message using seed: {seed}')
 
   for word in string:
       for character in word:
@@ -52,6 +72,16 @@ def encode(string, seed=None):
   return reverse(''.join(encoded))
 
 def decode(string, seed):
+  """
+  Decodes a string using a custom encryption method
+
+  Parameters:
+   - string (Str): The string being decoded
+   - seed (List[int]): The seed using during the encryption
+
+  Returns:
+   - Str: The decoded string
+  """
   if seed is None:
     print("Cannot run without a seed")
   string = reverse(string)
@@ -74,8 +104,8 @@ def decode(string, seed):
   
   return ''.join(decoded)
 
-seed = createSeed()
+seed = createSeed(5)
 
-string = encode("Hello World!", seed)
+string = encode("Goodbye World!", seed)
 print(string)
 print(decode(string, seed))
