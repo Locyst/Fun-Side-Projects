838eb2de97a25336fa8ef00ab20b1cf47798b719
Locyst <154724542+Locyst@users.noreply.github.com>
2024-02-20 16:22:10 -0700

    Update LocystFileSorter.py

--- LocystFileSorter.py
+++ LocystFileSorter.py
@@ -2,6 +2,7 @@ import os
 import time
 
 class LocystFileSort:
+    initized = False
     documentExtensions = ['.txt', '.md', '.pdf', '.doc']
     musicExtensions = ['.m4a', '.mp3', '.wav']
     videoExtensions = ['.mp4', '.avi', '.mkv', '.mov', '.wmv']
@@ -9,21 +10,27 @@ class LocystFileSort:
 
     homeDir = './Files' # Put the directory that this program is going to be placed in
     unsortedDir = 'Unsorted' # Put either your downloads folder or another file that is going to have all of the unsorted files
+    filePath = ''
 
     dirs = ['Documents', 'Pictures', 'Videos', 'Music',
       unsortedDir]
   
     @classmethod
     def init(cls):
+        cls.initized = True
         for directory in cls.dirs:
             path = os.path.join(cls.homeDir, directory)
             if not os.path.exists(path):
                 os.makedirs(path)
+        cls.filePath = f'{cls.homeDir}/{cls.unsortedDir}/{file}'
 
     @classmethod
     def sort(cls):
+        if cls.initized is False:
+            print("Run init before running other code")
+            return 0
+        
         for file in os.listdir(f'{cls.homeDir}/{cls.unsortedDir}'):
-            filePath = f'{cls.homeDir}/{cls.unsortedDir}/{file}'
             print(filePath)
 
             if os.path.isdir(filePath) is True:
@@ -32,13 +39,13 @@ class LocystFileSort:
             _, fileExtension = os.path.splitext(file)
 
             if fileExtension in cls.documentExtensions:
-                os.replace(filePath, f'{cls.homeDir}/Documents/{file}')
+                os.replace(cls.filePath, f'{cls.homeDir}/Documents/{file}')
             elif fileExtension in cls.musicExtensions:
-                os.replace(filePath, f'{cls.homeDir}/Music/{file}')
+                os.replace(cls.filePath, f'{cls.homeDir}/Music/{file}')
             elif fileExtension in cls.videoExtensions:
-                os.replace(filePath, f'{cls.homeDir}/Videos/{file}')
+                os.replace(cls.filePath, f'{cls.homeDir}/Videos/{file}')
             elif fileExtension in cls.imageExtensions:
-                os.replace(filePath, f'{cls.homeDir}/Pictures/{file}')
+                os.replace(cls.filePath, f'{cls.homeDir}/Pictures/{file}')
             else:
                 print(f"Unknown file: {file}")
                 print(fileExtension)
