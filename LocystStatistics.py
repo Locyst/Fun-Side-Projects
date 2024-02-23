e37961160823e99aa21abe5b3a7190953f3f4db2
Locyst <154724542+Locyst@users.noreply.github.com>
2024-02-23 14:08:19 -0700

    Update LocystStatistics.py

--- LocystStatistics.py
+++ LocystStatistics.py
@@ -1,2 +1,38 @@
-for i in range(10):
-  print(i)
+def organize(List):
+    return List.sort()
+
+def meanCalculate(List):
+  List = organize(List)
+  mean = 0
+
+  for num in List:
+    mean =+ num
+
+  mean / len(List)
+  
+  return mean
+  
+
+def modeCalculate(List):
+  List = organize(List)
+  mode = (0, 0)
+  
+  for num in List:
+    if num.count() > mode[1]:
+      mode = num, num.count()
+
+  return mode[0]
+  
+def medianCalculate(List):
+  List = organize(List)
+  return "I dunno the logic yet"
+  
+def rangeCalculate(List):
+  List = organize(List)
+  lowest = List[0]
+  highest = List[-1]
+
+  range = highest - lowest
+
+  return range
+  
