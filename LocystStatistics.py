5a1faf4d4c205c74c106302c6bed049bd558a4b1
Locyst <154724542+Locyst@users.noreply.github.com>
2024-02-23 14:52:19 -0700

    Update LocystStatistics.py

--- LocystStatistics.py
+++ LocystStatistics.py
@@ -1,28 +1,45 @@
 def organize(List):
     return sorted(List)
 
-def meanCalculate(List):
+def oddCheck(num):
+    if num % 2 == 0:
+        return False
+    else:
+        return True
+
+def meanCalculate(List): # Average
   List = organize(List)
   mean = 0
 
   for num in List:
-    mean =+ num
+    mean += num
 
-  mean / len(List)
+  mean = mean / len(List)
   
   return mean
   
 
-def modeCalculate(List):
+def modeCalculate(List): # Most Common
   List = organize(List)
 
   return max(set(List), key=List.count)
   
-def medianCalculate(List):
+def medianCalculate(List): # Middle
   List = organize(List)
+
+  if oddCheck(len(List)):
+      while len(List) > 1:
+          del List[0]
+          del List[-1]
+      return List[0]
+  else:
+      while len(List) > 2:
+          del List[0]
+          del List[-1]
+      return (((List[-1] - List[0]) / 2) + List[0])
   return "I dunno the logic yet"
   
-def rangeCalculate(List):
+def rangeCalculate(List): # Biggest - Smallest
   List = organize(List)
   lowest = List[0]
   highest = List[-1]
@@ -33,8 +50,8 @@ def rangeCalculate(List):
   
   
   
-def test(List = [1, 5, 2, 3, 6, 7, 3])
-    print(f'List: {List}')
+def test(List = [1, 5, 2, 3, 6, 7, 3]):
+    print(f'List: {List}\n')
     
     print(f'The mean is: {meanCalculate(List)}')
     print(f'The mode is: {modeCalculate(List)}')
